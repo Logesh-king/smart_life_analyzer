33a400fa3ad949141e34aed507a1efe5bbb1a186
Logesh-king <mllogesh2003@gmail.com>
2026-05-23 09:22:09 +0530

    23 date change

--- life_analyzer/settings.py
+++ life_analyzer/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STORAGES = {
